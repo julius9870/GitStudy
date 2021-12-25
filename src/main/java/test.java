96e056b04833550852884bfc034d4bd980ed7d5b
zjz <zjz@atguigu>
2021-12-25 21:23:03 +0800

    first commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -5,5 +5,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("first commit");
+        System.out.println("secound commit");
+        System.out.println("hot-fix");
     }
 }
